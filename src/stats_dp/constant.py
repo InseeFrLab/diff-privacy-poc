5d217b5f56dc5d0127127a164ed076d3c94872f5
stuartbenoliel <stuart.benoliel@eleve.ensai.fr>
2025-08-14 09:03:10 +0000

    build push ...

--- src/stats_dp/constant.py
+++ src/stats_dp/constant.py
@@ -3,9 +3,6 @@ import os
 
 # Stockage des options S3 (MinIO)
 storage_options: dict[str, str] = {
-#    "aws_access_key_id": os.environ["AWS_ACCESS_KEY_ID"],
-#    "aws_secret_access_key": os.environ["AWS_SECRET_ACCESS_KEY"],
-#    "aws_session_token": os.environ["AWS_SESSION_TOKEN"],
     "endpoint_url": "https://minio.lab.sspcloud.fr",
 }
 
